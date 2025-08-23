e0f6bdb883ee0885be1908079a7480b02c919f70
nayeon <nayeon22@g.skku.edu>
2025-08-23 22:08:58 +0900

    feat: add computer name display feature

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+import socket # 1. app.py에 socket 모듈을 import 한다.
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '홈페이지입나다!'
+    # 2. render_template 호출 이전에 코드를 추가
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    # 3. render_template 호출 시 computername=hostname 인자를 추가
+    return render_template('menu.html', computername=hostname)
 
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # 5. app.run() 실행 시 debug=True 옵션을 설정
